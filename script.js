f4b660b6e6f49d3c853c6a05c87bd2396184d32e
Zahra <zahra.shahabi.dk@gmail.com>
2023-01-30 19:58:51 +0100

    Define and add array to the project

---
+++ script.js
@@ -0,0 +1,12 @@
+const citates = [
+"Et lands opfindelser fortæller meget om dets beboere. Det er næppe tilfældigt, at det var danskerne, som opfandt antabus og korkbæltet – Paul Hammerich",
+"Vi ender som en flok folkedansere, hotelkarle og ludere, hvis vi ikke gør noget ved det – Ove Sprogøe (Egon Olsen i Olsenbanden)",
+"De vanskeligste år i en danskers liv er tiden, fra man forlader folkeskolen, til folkepensionen kan hæves – Ukendt",
+"Det danske klima er stort set kun ideelt for læger – H. Juulsgaard",
+"Jo, vi er verdens bedste land, hvor få har for meget og færre format – Poul Hammerich",
+"Som dansker bør man af og til rive sig løs fra det jævne og opsøge bjerge, træne sig i udsyn og svimmelhed – Benny Andersen",
+"Indstillingen som går gennem hele danmarkshistorien, er denne: Lad os nu vente og se, hvad det kan blive til – Palle Lauring",
+"I Danmark kan alle analfabeter læse og skrive – Poul Henningsen",
+"En dansker er liges0m en flaske kethcup – først kommer der ikke noget – og så kommer det hele på én gang – Kjeld Abell",
+"Den, som ikke forstår spøg, forstår ikke dansk – Georg Brandes"
+]
